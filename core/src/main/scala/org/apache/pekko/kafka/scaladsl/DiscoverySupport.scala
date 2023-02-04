a5a58e3329775f4e254e971d85ad895ac80e65c4
Seeta Ramayya Vadali <3521036+Seetaramayya@users.noreply.github.com>
2023-02-04 11:30:49 +0100

    fixed pekko discovery check

--- core/src/main/scala/org/apache/pekko/kafka/scaladsl/DiscoverySupport.scala
+++ core/src/main/scala/org/apache/pekko/kafka/scaladsl/DiscoverySupport.scala
@@ -114,10 +114,10 @@ object DiscoverySupport {
   }
 
   private def checkClassOrThrow(system: ActorSystemImpl): Unit =
-    system.dynamicAccess.getClassFor("akka.discovery.Discovery$") match {
+    system.dynamicAccess.getClassFor("org.apache.pekko.discovery.Discovery$") match {
       case Failure(_: ClassNotFoundException | _: NoClassDefFoundError) =>
         throw new IllegalStateException(
-          s"Akka Discovery is being used but the `akka-discovery` library is not on the classpath, it must be added explicitly. See https://doc.akka.io/docs/alpakka-kafka/current/discovery.html")
+          s"Pekko Discovery is being used but the `pekko-discovery` library is not on the classpath, it must be added explicitly. See https://pekko.apache.org/docs/pekko/current/discovery/index.html")
       case _ =>
     }
 
