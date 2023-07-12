0c36b2a2f8a2208aa3c3afd973e966b362920cdb
PJ Fanning <pjfanning@users.noreply.github.com>
2023-07-12 10:18:50 +0100

    update kafka license
    Update KafkaClusterSharding.scala

--- cluster-sharding/src/main/scala/org/apache/pekko/kafka/cluster/sharding/KafkaClusterSharding.scala
+++ cluster-sharding/src/main/scala/org/apache/pekko/kafka/cluster/sharding/KafkaClusterSharding.scala
@@ -12,6 +12,25 @@
  * Copyright (C) 2016 - 2020 Lightbend Inc. <https://www.lightbend.com>
  */
 
+// license header from
+// https://github.com/apache/kafka/blob/451fff8937c626a187371b8fcdf50cfb5177d38f/clients/src/main/java/org/apache/kafka/clients/producer/internals/DefaultPartitioner.java
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.pekko.kafka.cluster.sharding
 
 import java.util.concurrent.{ CompletionStage, ConcurrentHashMap }
