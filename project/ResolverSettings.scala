5cf089fe0fc0291b7633c55145a43dc9fc5f5071
PJ Fanning <pjfanning@users.noreply.github.com>
2024-03-10 14:09:02 +0100

    remove jcenter repo

--- project/ResolverSettings.scala
+++ project/ResolverSettings.scala
@@ -12,8 +12,4 @@ import sbt._
 object ResolverSettings {
   lazy val testSpecificResolvers = Seq(
     "Confluent Maven Repo" at "https://packages.confluent.io/maven/")
-
-  lazy val projectResolvers = Seq(
-    // for Jupiter interface (JUnit 5)
-    Resolver.jcenterRepo)
 }
