0c0121edd9bac57b4a288be44fa9a87e442b693b
PJ Fanning <pjfanning@users.noreply.github.com>
2024-09-16 10:42:57 +0100

    scala 2.12.20 (#226)

--- project/Versions.scala
+++ project/Versions.scala
@@ -17,7 +17,7 @@ object Versions {
 
   // align ignore-prefixes in scripts/link-validator.conf
   val Scala213 = "2.13.14" // update even in link-validator.conf
-  val Scala212 = "2.12.19"
+  val Scala212 = "2.12.20"
   val Scala3 = "3.3.3"
 
   val pekkoVersionForDocs = "current"
