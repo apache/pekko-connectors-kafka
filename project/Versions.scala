23bd6d7167cd3f1a9c406ac8b23197c5adb2f9db
Scala Steward <scala_steward@virtuslab.com>
2026-09-04 15:06:36 +0000

    Update scala-library to 3.9.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object Versions {
   val Nightly: Boolean = sys.env.get("EVENT_NAME").contains("schedule")
 
   // align ignore-prefixes in scripts/link-validator.conf
-  val Scala213 = "2.13.18" // update even in link-validator.conf
+  val Scala213 = "3.9.0" // update even in link-validator.conf
   val Scala3 = "3.3.8"
   val Scala3Next = "3.8.4"
   val PublishedScalaVersions = Seq(Scala213, Scala3)
